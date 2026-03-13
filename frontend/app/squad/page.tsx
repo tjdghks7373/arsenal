0b888de29ad3bf9ac64a5d00962de78339a36f00
kim <tjdghks7373@naver.com>
2026-03-13 14:30:47 +0900

    팀 스쿼드 수정

--- frontend/app/squad/page.tsx
+++ frontend/app/squad/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 
 interface Player {
   back_number: string;
@@ -10,33 +10,27 @@ interface Player {
 }
 
 export default function SquadPage() {
-  const [players, setPlayers] = useState<Player[]>([]);
-  const [loading, setLoading] = useState(true);
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["players"], // 데이터의 고유 이름
+    queryFn: () =>
+      fetch("https://arsenal-cy44.onrender.com/api/players").then((res) =>
+        res.json(),
+      ),
+  });
 
-  useEffect(() => {
-    fetch("https://arsenal-cy44.onrender.com/api/players") // http://127.0.0.1:8000/api/players
-      .then((res) => res.json())
-      .then((data) => {
-        const sorted = [...data.players].sort(
-          (a, b) => parseInt(a.back_number) - parseInt(b.back_number),
-        );
-        setPlayers(sorted);
-        setLoading(false);
-      })
-      .catch((err) => {
-        console.error("데이터 로드 실패:", err);
-        setLoading(false);
-      });
-  }, []);
+  if (isLoading) return <div>선수단 정보를 불러오는 중...</div>;
+  if (error) return <div>에러 발생!</div>;
 
-  if (loading) return <div>로딩 중...</div>;
+  const sortedPlayers = [...data.players].sort(
+    (a, b) => parseInt(a.back_number) - parseInt(b.back_number),
+  );
 
   return (
     <div>
-      <h1>Arsenal 선수단 (등번호 순)</h1>
-      {players.map((player: Player) => (
+      <h1>Arsenal 선수단 (React Query 적용!)</h1>
+      {sortedPlayers.map((player: Player) => (
         <div key={player.back_number}>
-          {player.back_number}: {player.name} : {player.age}
+          {player.back_number}: {player.name}
         </div>
       ))}
     </div>
